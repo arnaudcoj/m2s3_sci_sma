c2ef977bf72855587069d2cd2ed0adac6c920bbf
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2016-09-08 15:59:26 +0200

    add func getNbRow, getNbCol, getFreeCells and printASCII

--- Environment.py
+++ Environment.py
@@ -2,10 +2,53 @@ class Environment(object):
     """docstring for Environment"""
     def __init__(self, gridSizeX, gridSizeY, torus):
         super(Environment, self).__init__()
-        self.init_grid(gridSizeX, gridSizeY, torus)
+        self.initGrid(gridSizeX, gridSizeY, torus)
 
-    def init_grid(self, gridSizeX, gridSizeY, torus):
+    def initGrid(self, gridSizeX, gridSizeY, torus):
         self.grid = []
-        for j in range(gridSizeY):
-            self.grid.append([None] * gridSizeX)
+        for i in range(gridSizeX):
+            self.grid.append([None] * gridSizeY)
+        self.grid[0][1] = True
         print("The grid of size", gridSizeX, gridSizeY, "has successfully been created.")
+
+    def getFreeCells(self):
+        freeCells = []
+        for i in range(self.getNbCol()):
+            for j in range(self.getNbRow()):
+                if self.grid[i][j] == None:
+                    freeCells.append((i,j))
+        return freeCells
+
+    def getNbRow(self):
+        return len(self.grid[0])
+
+    def getNbCol(self):
+        return len(self.grid)
+
+    def printASCII(self):
+        print(self.getNbCol(), self.getNbRow())
+
+        #First Line
+        print("+", end="")
+        for i in range(self.getNbCol()):
+            print("-+", end="")
+
+        #Intermediate Lines
+        for j in range(self.getNbRow()):
+            print("\n|", end="")
+            for i in range(self.getNbCol()):
+                if self.grid[i][j] :
+                    print("X", end="")
+                else:
+                    print(" ", end="")
+                print("|", end="")
+            print("\n+", end="")
+            for i in range(self.getNbCol()):
+                print("-+", end="")
+
+        #End
+        print("")
+
+#env = Environment(4,2,True)
+#env.printASCII()
+#print(env.getFreeCells())
