31a571764077fee98b03eb40d903193e1e70433f
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2016-09-19 15:10:33 +0200

    move randomPas from Particle to Agent

--- particles/Particle.py
+++ particles/Particle.py
@@ -10,21 +10,6 @@ class Particle(Agent):
         self.setRandomColor()
         self.setRandomPas()
 
-    def setRandomColor(self):
-        de = ("%02x" %random.randint(0,255))
-        re = ("%02x" %random.randint(0,255))
-        we = ("%02x" %random.randint(0,255))
-        ge = "#"
-        self.color= ge + de + re + we
-
-    def setRandomPas(self):
-        # maybe a bit gridy but we want to avoid a (0,0) direction
-        pasList = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
-        random.shuffle(pasList)
-        pas = pasList[0]
-        self.pasX = pas[0]
-        self.pasY = pas[1]
-
     def decide(self):
         nextCoords = self.findNextCell()
         dirChanged = False
