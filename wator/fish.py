6fb4fde93bc09dccdf223d363b0b4376b86b97ee
Kaendan <tristan.camus@gmail.com>
2016-09-19 10:49:08 +0200

    Prepared wator

---
+++ wator/fish.py
@@ -0,0 +1,11 @@
+from Agent import *
+
+class Fish(Agent):
+	def __init__(self, environment, posX, posY, name, torus, trace):
+        super(Particle, self).__init__(environment, posX, posY, name, torus, trace)
+
+	def create(self):
+        raise NotImplementedError("Agent.create needs to be implemented")
+
+    def decide(self):
+        raise NotImplementedError("Agent.decide needs to be implemented")
