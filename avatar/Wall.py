41d315aa5c5ea10706d6f7f5484e7a1ff94ab632
arnaudcoj <arnaud.cojez@univ-lille1.fr>
2016-09-28 22:16:09 +0200

    fix wall constructor

--- avatar/Wall.py
+++ avatar/Wall.py
@@ -2,8 +2,8 @@ from Agent import *
 
 class Wall(object):
     """docstring for Wall."""
-    def __init__(self, arg):
-        super(Wall, self).__init__()
+    def __init__(self, environment, posX, posY, name):
+        super(Wall, self).__init__(environment, posX, posY, name)
 
     def decide(self):
         pass
