46365b0ec6b2a0f822cf6c7a4fe443189e540a9d
arnaudcoj <arnaud.cojez@univ-lille1.fr>
2016-09-28 22:06:05 +0200

    add wall to avatar

---
+++ avatar/Wall.py
@@ -0,0 +1,12 @@
+from Agent import *
+
+class Wall(object):
+    """docstring for Wall."""
+    def __init__(self, arg):
+        super(Wall, self).__init__()
+
+    def decide(self):
+        pass
+
+    def update(self):
+        pass
